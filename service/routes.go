d0b3106f583c0d6ab79e9df42098cfa17300375c
John Calabrese <jcalabrese@pivotal.io>
2015-04-01 14:20:28 -0400

    [#90788386] updating routes and pipeline
    adding some more boilerplate stuff to the routing
    and re-enabling docker in pipeline, as
    wercker says it should be working again....we shall see

--- service/routes.go
+++ service/routes.go
@@ -2,32 +2,37 @@ package pezauth
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 )
 
 //Constants to construct routes with
 const (
 	APIVersion1 = "v1"
 	AuthGroup   = "auth"
+	APIKey      = "/api-key/:user"
+	APIKeys     = "/api-keys"
+	StaticPath  = "public"
 )
 
 //formatted strings based on constants, to be used in URLs
 var (
 	URLAuthBaseV1 = fmt.Sprintf("/%s/%s", APIVersion1, AuthGroup)
-	APIKey        = "/api-key/:user"
-	APIKeys       = "/api-keys"
 )
 
-func fakeController(params martini.Params) (int, string) {
-	return 200, "success"
+func fakeController(params martini.Params, log *log.Logger, r render.Render) {
+	r.JSON(200, map[string]interface{}{"hello": "world"})
 }
 
 //InitRoutes - initialize the mappings for controllers against valid routes
 func InitRoutes(m *martini.ClassicMartini) {
+	m.Use(martini.Static(StaticPath))
+
 	m.Group("/", func(r martini.Router) {
-		r.Get("info", func() string {
-			return "auth service"
+		r.Get("info", func() (int, string) {
+			return 200, "auth service"
 		})
 	})
 
