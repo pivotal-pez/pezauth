52f6ff4d8378f0f47900bbefd3037638f2f27414
John Calabrese <jcalabrese@pivotal.io>
2015-04-02 16:54:23 -0400

    [#90788386] cleaning URI version info
    the uri has the version info in it, which
    causes a conflict when routing to the sso
    domain on file. filter out the version and
    we are all good

--- service/auth.go
+++ service/auth.go
@@ -3,6 +3,8 @@ package pezauth
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 
 	cfenv "github.com/cloudfoundry-community/go-cfenv"
 	"github.com/go-martini/martini"
@@ -11,6 +13,7 @@ import (
 	goauth2 "golang.org/x/oauth2"
 )
 
+//Constants to construct my oauth calls
 const (
 	ClientID      = "1083030294947-6g3bhhrgl3s7ul736jet625ajvp94f5p.apps.googleusercontent.com"
 	ClientSecret  = "kfgM5mT3BqPQ84VeXsYokAK_"
@@ -18,9 +21,22 @@ const (
 	sessionSecret = "shhh.donttellanyone"
 )
 
+//Vars for my oauth calls
 var (
 	Scopes = []string{"https://www.googleapis.com/auth/plus.me"}
 )
+
+func cleanVersionFromURI(uri string) string {
+	var digitsRegexp = regexp.MustCompile(`-.*?\.`)
+	match := digitsRegexp.FindStringSubmatch(uri)
+
+	if len(match) > 0 {
+		newS := strings.Replace(uri, match[0], ".", -1)
+		fmt.Println(newS)
+		uri = newS
+	}
+	return uri
+}
 
 func getAppEnv() (appEnv *cfenv.App) {
 	var (
@@ -29,7 +45,12 @@ func getAppEnv() (appEnv *cfenv.App) {
 
 	switch os.Getenv("LOCAL") {
 	case "true":
-		appEnv = &cfenv.App{ApplicationURIs: []string{fmt.Sprintf("http://localhost:%s", os.Getenv("PORT"))}}
+		appEnv = &cfenv.App{
+			ApplicationURIs: []string{
+				fmt.Sprintf("http://localhost-lkashdgaskhdglaskdhgasd:%s", os.Getenv("PORT")),
+				fmt.Sprintf("http://localhost:%s", os.Getenv("PORT")),
+			},
+		}
 
 	default:
 		if appEnv, err = cfenv.Current(); err != nil {
@@ -39,16 +60,20 @@ func getAppEnv() (appEnv *cfenv.App) {
 	return
 }
 
+func getAppURI() string {
+	appEnv := getAppEnv()
+	return cleanVersionFromURI(appEnv.ApplicationURIs[0])
+}
+
 //InitAuth - initializes authentication middleware for controllers
 func InitAuth(m *martini.ClassicMartini) {
-	appEnv := getAppEnv()
 	m.Use(sessions.Sessions(sessionName, sessions.NewCookieStore([]byte(sessionSecret))))
 	m.Use(oauth2.Google(
 		&goauth2.Config{
 			ClientID:     ClientID,
 			ClientSecret: ClientSecret,
 			Scopes:       Scopes,
-			RedirectURL:  fmt.Sprintf("%s/oauth2callback", appEnv.ApplicationURIs[0]),
+			RedirectURL:  fmt.Sprintf("%s/oauth2callback", getAppURI()),
 		},
 	))
 	m.Use(oauth2.LoginRequired)
