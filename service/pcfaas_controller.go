175fbefe23f56f35596f9bec75a24fc6464e7acc
Kevin Hoffman <kehoffman@pivotal.io>
2015-09-02 09:40:36 -0400

    Formatted file per go standards.

--- service/pcfaas_controller.go
+++ service/pcfaas_controller.go
@@ -7,23 +7,23 @@ import (
 	"github.com/martini-contrib/render"
 )
 
-//NewMeController - a controller for me requests
+//NewPcfaasController - a controller for inventory requests
 func NewPcfaasController() Controller {
 	return new(pcfaasController)
 }
 
-//Get - get a get handler for authkeyv1
+// Get - gets a handler for inventory requests
 func (s *pcfaasController) Get() interface{} {
 	var handler PcfaasGetInventoryHandler = func(log *log.Logger, r render.Render, tokens oauth2.Tokens) {
 		userInfo := GetUserInfo(tokens)
 		log.Println("getting userInfo: ", userInfo)
-    statusCode := SuccessStatus
-	  invItem := InventoryItem{SKU:"2C.small",Tier:"2",OfferingType:"C",Size:"small",Status:"available",ID:"abc123guid"}
-    invItem2 := InventoryItem{SKU:"2C.small", Tier:"2", OfferingType:"C", Size:"small", Status:"leased", ID:"abc32123guid"}
-    items := make([]InventoryItem, 2)
-    items[0] = invItem
-    items[1] = invItem2
-    r.JSON(statusCode, items)
+		statusCode := SuccessStatus
+		invItem := InventoryItem{SKU: "2C.small", Tier: "2", OfferingType: "C", Size: "small", Status: "available", ID: "abc123guid"}
+		invItem2 := InventoryItem{SKU: "2C.small", Tier: "2", OfferingType: "C", Size: "small", Status: "leased", ID: "abc32123guid"}
+		items := make([]InventoryItem, 2)
+		items[0] = invItem
+		items[1] = invItem2
+		r.JSON(statusCode, items)
 	}
 	return handler
 }
