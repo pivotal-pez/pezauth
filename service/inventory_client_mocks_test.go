b9b0a84c42b4fe4832bec328ac96935ff2d72920
Kevin Hoffman <kehoffman@pivotal.io>
2015-09-11 13:31:25 -0400

    Added a unit test for querying leases. Moved unit tests out of integration area since they all work on mocks.

---
+++ service/inventory_client_mocks_test.go
@@ -0,0 +1,75 @@
+package pezauth_test
+
+const (
+  TwoItemsSample = `{
+  "status": "success",
+  "data": [
+    {
+      "id": "55f077966696f40147000001",
+      "sku": "2C.small",
+      "tier": 0,
+      "offeringType": "",
+      "size": "",
+      "attributes": {},
+      "status": "",
+      "lease_id": ""
+    },
+    {
+      "id": "55f0779a6696f40147000002",
+      "sku": "2C.small",
+      "tier": 0,
+      "offeringType": "",
+      "size": "",
+      "attributes": {},
+      "status": "",
+      "lease_id": ""
+    }
+  ]
+}`
+
+  LeasedItemsSample = `{
+  "status": "success",
+  "data": [
+    {
+      "id": "55f1dff033d183001d000001",
+      "sku": "2C.small",
+      "tier": 2,
+      "offering_type": "C",
+      "size": "small",
+      "attributes": {},
+      "status": "available",
+      "lease_id": ""
+    },
+    {
+      "id": "55f230977bed08001d000001",
+      "sku": "2C.small",
+      "tier": 2,
+      "offering_type": "C",
+      "size": "small",
+      "attributes": {},
+      "status": "leased",
+      "lease_id": "55f22fe5b0cc8b001d000001"
+    }
+  ]
+}`
+
+ IndividualLease = `{
+  "status": "success",
+  "data": {
+    "id": "55f22fe5b0cc8b001d000001",
+    "inventory_item_id": "",
+    "user": "dnem",
+    "duration": "28 days",
+    "start_date": "2015-09-01",
+    "end_date": "2015-09-29",
+    "status": "active",
+    "attributes": {}
+  }
+}`
+
+  NoInventoryDataSample = `{
+    "status" : "fail",
+    "message" : "Things went horribly wrong"
+  }`
+
+)
--- service/inventory_client_test.go
+++ service/inventory_client_test.go
@@ -1,11 +1,12 @@
-package integrations_test
+package pezauth_test
 
 import (
-//	"log"
+	"log"
 //	"os"
 	"fmt"
    "net/http"
 	 "net/http/httptest"
+	 "strings"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/pivotal-pez/pezauth/integrations"
@@ -18,6 +19,19 @@ func makeServer(payload string) *httptest.Server {
 
 			fmt.Fprintln(w, payload)
 			}))
+}
+
+func makeLeaseAvailableServer(payloadFirst string, payloadSecond string) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+			w.Header().Set("Content-Type", "application/json")
+
+			if strings.Contains(r.RequestURI, "lease") {
+				fmt.Fprintln(w, payloadSecond)
+			} else {
+				fmt.Fprintln(w, payloadFirst)
+			}
+		}))
 }
 
 var _ = Describe("Inventory Service Client", func() {
@@ -43,6 +57,18 @@ var _ = Describe("Inventory Service Client", func() {
 	    results, err := invClient.GetInventoryItems()
 			Ω(len(results)).Should(Equal(0))
 			Ω(err.Error()).Should(Equal("Things went horribly wrong"))
+		})
+
+		It("should query for a lease when appropriate", func() {
+				server := makeLeaseAvailableServer(LeasedItemsSample, IndividualLease)
+				defer server.Close()
+				rootURL := server.URL
+				invClient := new(MyInventoryClient).NewWithURL(rootURL)
+				results, err := invClient.GetInventoryItems()
+				Ω(len(results)).Should(Equal(2))
+				Ω(err).Should(BeNil())
+				log.Printf("Detected user owning lease %s", results[1].CurrentLease.Username)
+				Ω(results[1].CurrentLease.Username).Should(Equal("dnem"))
 		})
 	})
 })
